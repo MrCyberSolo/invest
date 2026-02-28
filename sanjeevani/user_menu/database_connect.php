55c914f44651d09ed03f7b5c6db9156ce4e77f29
mrcybersolo <kushalkoli02@gmail.com>
2026-02-28 22:45:30 +0530

    db

--- sanjeevani/user_menu/database_connect.php
+++ sanjeevani/user_menu/database_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
 $servername = "localhost";
-$username = "u515176669_demo34";
-$password = "2;ic362;=M";
-$database = "u515176669_demo34";
+$username = "u415747521_invest";
+$password = "U415747521_invest";
+$database = "u415747521_invest";
 
 $con = mysqli_connect($servername, $username, $password, $database);
 
